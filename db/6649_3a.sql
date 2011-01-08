1cb9e9911045a0d58807dbdb52c345b6e0b123f4
Hans Hoermann <support@hans-hoermann.de>
2011-01-08 20:17:40 +0100

    re1 + nn1 Beispiel begonnen

--- db/6649_3a.sql
+++ db/6649_3a.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `Spezl`;
 CREATE TABLE `Spezl` (
   `ID` int(11) NOT NULL auto_increment,
   `Spezl` varchar(255) default NULL,
+  `maennlich` enum('ja','nein') NOT NULL DEFAULT 'ja',
   PRIMARY KEY  (`ID`),
-  UNIQUE KEY `IDX` (`Spezl`)
+  UNIQUE KEY `IDX` (`Spezl`,maennlich)
 ) TYPE=MyISAM;
-
--- db/6649_4.sql
+++ db/6649_4.sql
@@ -1,8 +1,9 @@
 DROP TABLE IF EXISTS `Biergarten`;
 CREATE TABLE `Biergarten` (
   `ID` int(11) NOT NULL auto_increment,
+  `OrtsteilID` int(11) NOT NULL DEFAULT '1',
   `Biergarten` varchar(255) default NULL,
   PRIMARY KEY  (`ID`),
-  UNIQUE KEY `IDX` (`Biergarten`)
+  UNIQUE KEY `IDX` (`Biergarten`,OrtsteilID)
 ) TYPE=MyISAM;
 
