16bb9ed90097ac8d7382d6f5ce1245e55d76aa30
Hans Hoermann <support@hans-hoermann.de>
2013-01-24 23:49:00 +0100

    1:N Formular: Eingabefelder mit CSS und fieldset/label/ul/li ausrichten.

--- db/erzeugenUser.sql
+++ db/erzeugenUser.sql
@@ -2,10 +2,10 @@ use mysql;
 
 # /////////////// Tabelle 'user' //////////////////////////////////
 # ////////////////////////////////////////////////////////////////
-INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','wrzl', password('prmft'));
+INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','imzad', password('prmft'));
 
 # /////////////// Tabelle 'db' ////////////////////////////////////
 # ////////////////////////////////////////////////////////////////
-INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','erica','wrzl','Y','Y','Y','Y','Y');
+INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','sahara','imzad','Y','Y','Y','Y','Y');
 
 FLUSH PRIVILEGES;
