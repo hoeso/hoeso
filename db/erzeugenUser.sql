03606ab82aeafdd729998b7a9d149ce4f0f758dc
Hans Hoermann <info@hoermann-solutions.com>
2014-07-29 23:18:49 +0200

    FBS 2.0 anzeigendZieltabelle implementiert. Noch ohne Summenanzeige am Ende in der rechten Ecke.

--- db/erzeugenUser.sql
+++ db/erzeugenUser.sql
@@ -2,10 +2,12 @@ use mysql;
 
 # /////////////// Tabelle 'user' //////////////////////////////////
 # ////////////////////////////////////////////////////////////////
-INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','aide', password('prmft'));
+INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','imzad', password('prmft'));
+INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','sahara', password(''));
 
 # /////////////// Tabelle 'db' ////////////////////////////////////
 # ////////////////////////////////////////////////////////////////
-INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','ass21','aide','Y','Y','Y','Y','Y');
+INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','sahara','imzad','Y','Y','Y','Y','Y');
+INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','sahara','sahara','Y','N','N','N','Y');
 
 FLUSH PRIVILEGES;
