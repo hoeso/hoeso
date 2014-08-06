019866da2bc8b6c1285189efad04a89fce2f2eb7
Hans Hoermann <info@hoermann-solutions.com>
2014-08-06 21:53:26 +0200

    Angebot erstellen am Einbauen in FBS2.

--- db/erzeugenUser.sql
+++ db/erzeugenUser.sql
@@ -2,22 +2,12 @@ use mysql;
 
 # /////////////// Tabelle 'user' //////////////////////////////////
 # ////////////////////////////////////////////////////////////////
-<<<<<<< HEAD
-INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','imzad', password('prmft'));
-INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','sahara', password(''));
+INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','phyll', password('prmft'));
+INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','pv', password(''));
 
 # /////////////// Tabelle 'db' ////////////////////////////////////
 # ////////////////////////////////////////////////////////////////
-INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','sahara','imzad','Y','Y','Y','Y','Y');
-INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','sahara','sahara','Y','N','N','N','Y');
-=======
-INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','aide', password('prmft'));
-INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','ass21', password(''));
-
-# /////////////// Tabelle 'db' ////////////////////////////////////
-# ////////////////////////////////////////////////////////////////
-INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','ass21','aide','Y','Y','Y','Y','Y');
-INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Lock_tables_priv) VALUES ('localhost','ass21','ass21','Y','Y');
->>>>>>> 5d0b4875f810a08c89f752a922aca4a13cea6daa
+INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','pv','phyll','Y','Y','Y','Y','Y');
+INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','pv','pv','Y','N','N','N','Y');
 
 FLUSH PRIVILEGES;
