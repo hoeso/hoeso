aced85c9b1644cb94fd4c7a3dd20e428129788db
Hans Hoermann <info@hoermann-solutions.com>
2015-07-07 20:48:45 +0000

    Ergaenzungen, die eine Inbetriebnahme unter apache/userdir mit php und mysql erleichtern.

--- db/erzeugenUser.sql
+++ db/erzeugenUser.sql
@@ -6,6 +6,6 @@ INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','aide', pass
 
 # /////////////// Tabelle 'db' ////////////////////////////////////
 # ////////////////////////////////////////////////////////////////
-INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','ass21','aide','Y','Y','Y','Y','Y');
+INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','kn','aide','Y','Y','Y','Y','Y');
 
 FLUSH PRIVILEGES;
