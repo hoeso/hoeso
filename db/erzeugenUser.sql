90ea48f7a2eaba6294dc70a7808fe4aeb6bda565
Hans Hoermann <info@hoermann-solutions.com>
2015-07-28 15:20:47 +0200

    intelligentes Fortsetzen eingeführt

--- db/erzeugenUser.sql
+++ db/erzeugenUser.sql
@@ -2,12 +2,12 @@ use mysql;
 
 # /////////////// Tabelle 'user' //////////////////////////////////
 # ////////////////////////////////////////////////////////////////
-INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','phyll', password('prmft'));
-INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','pv', password(''));
+INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','zettl', password('straum'));
+INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','kn', password(''));
 
 # /////////////// Tabelle 'db' ////////////////////////////////////
 # ////////////////////////////////////////////////////////////////
-INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','pv','phyll','Y','Y','Y','Y','Y');
-INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','pv','pv','Y','N','N','N','Y');
+INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','kn','zettl','Y','Y','Y','Y','Y');
+INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','kn','kn','Y','N','N','N','Y');
 
 FLUSH PRIVILEGES;
