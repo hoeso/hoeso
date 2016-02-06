89c25a6e2057150419ba4abc3e15d4c20c242cfb
Hans Hoermann <info@hoermann-solutions.com>
2016-02-06 11:46:06 +0100

    Client, Adresse, Beziehung, Zeitstempel anlegbar.

--- db/cfs.sql
+++ db/cfs.sql
@@ -1,19 +1,79 @@
+-- MySQL dump 10.13  Distrib 5.6.28, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: cfs
+-- ------------------------------------------------------
+-- Server version	5.6.28-1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Beziehung`
+--
+
+DROP TABLE IF EXISTS `Beziehung`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Beziehung` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Beziehung` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `IDX` (`Beziehung`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Beziehung`
+--
+
+LOCK TABLES `Beziehung` WRITE;
+/*!40000 ALTER TABLE `Beziehung` DISABLE KEYS */;
+INSERT INTO `Beziehung` VALUES (1,'Vater'),(2,'Mutter'),(3,'Tochter'),(4,'Sohn'),(5,'Neffe'),(6,'Nichte'),(7,'Tante'),(8,'Onkel'),(9,'Client');
+/*!40000 ALTER TABLE `Beziehung` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Client`
+--
+
 DROP TABLE IF EXISTS `Client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Client` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `geborenAm` date DEFAULT '1901-01-01',
-  `Geschlecht` enum('weiblich','männlich') NOT NULL DEFAULT 'weiblich',
+  `Geschlecht` enum('weiblich','maennlich') NOT NULL DEFAULT 'weiblich',
+  `Name` varchar(80) DEFAULT NULL,
+  `Vorname` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS `ClientBeziehung`;
-CREATE TABLE `ClientBeziehung` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Beziehung` varchar(255) DEFAULT 'Kind',
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+--
+-- Dumping data for table `Client`
+--
+
+LOCK TABLES `Client` WRITE;
+/*!40000 ALTER TABLE `Client` DISABLE KEYS */;
+INSERT INTO `Client` VALUES (1,'1901-01-01','weiblich','Meisel','Inge');
+/*!40000 ALTER TABLE `Client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ClientAdresse`
+--
 
 DROP TABLE IF EXISTS `ClientAdresse`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ClientAdresse` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `ClientID` int(11) NOT NULL DEFAULT '1',
@@ -23,12 +83,74 @@ CREATE TABLE `ClientAdresse` (
   `Ort` varchar(120) DEFAULT NULL,
   `Telefon` varchar(255) DEFAULT '',
   PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ClientAdresse`
+--
+
+LOCK TABLES `ClientAdresse` WRITE;
+/*!40000 ALTER TABLE `ClientAdresse` DISABLE KEYS */;
+INSERT INTO `ClientAdresse` VALUES (1,1,1,'KurfÃ¼rstendamm 19-24','10719','Berlin','030-1');
+/*!40000 ALTER TABLE `ClientAdresse` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ClientBeziehung`
+--
+
+DROP TABLE IF EXISTS `ClientBeziehung`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ClientBeziehung` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Beziehung` varchar(255) DEFAULT 'Kind',
+  PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ClientBeziehung`
+--
+
+LOCK TABLES `ClientBeziehung` WRITE;
+/*!40000 ALTER TABLE `ClientBeziehung` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ClientBeziehung` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Zeitstempel`
+--
 
 DROP TABLE IF EXISTS `Zeitstempel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Zeitstempel` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Datum` date DEFAULT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `Zeitstempel` (`Datum`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Zeitstempel`
+--
+
+LOCK TABLES `Zeitstempel` WRITE;
+/*!40000 ALTER TABLE `Zeitstempel` DISABLE KEYS */;
+INSERT INTO `Zeitstempel` VALUES (1,'2016-01-01');
+/*!40000 ALTER TABLE `Zeitstempel` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-02-06 11:43:58
