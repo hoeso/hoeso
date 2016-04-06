a7fd179ec90a51a6fccc27042c7ab0dbb19a717c
Hans Hoermann <info@hoermann-solutions.com>
2016-04-06 18:20:03 +0200

    KW umschaltbar zwischen Client u. MA. Es fehlen die MA-Links.

--- db/cfs.sql
+++ db/cfs.sql
@@ -144,8 +144,33 @@ CREATE TABLE `ClientVS` (
 
 LOCK TABLES `ClientVS` WRITE;
 /*!40000 ALTER TABLE `ClientVS` DISABLE KEYS */;
-INSERT INTO `ClientVS` VALUES (10,4,1,33,'6'),(11,4,4,64,'8'),(12,4,1,86,'7'),(13,4,2,27,'8'),(14,3,1,25,'2'),(15,3,2,25,'4');
+INSERT INTO `ClientVS` VALUES (10,4,1,33,'6'),(11,4,4,63,'8'),(12,4,1,85,'7'),(13,4,2,27,'8'),(14,3,1,25,'2'),(15,3,2,25,'4');
 /*!40000 ALTER TABLE `ClientVS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Engagement`
+--
+
+DROP TABLE IF EXISTS `Engagement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Engagement` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Engagement` enum('0%','1%','2%','3%','4%','5%','6%','7%','8%','9%','10%','11%','12%','13%','14%','15%','16%','17%','18%','19%','20%','21%','22%','23%','24%','25%','26%','27%','28%','29%','30%','31%','32%','33%','34%','35%','36%','37%','38%','39%','40%','41%','42%','43%','44%','45%','46%','47%','48%','49%','50%','51%','52%','53%','54%','55%','56%','57%','58%','59%','60%','61%','62%','63%','64%','65%','66%','67%','68%','69%','70%','71%','72%','73%','74%','75%','76%','77%','78%','79%','80%','81%','82%','83%','84%','85%','86%','87%','88%','89%','90%','91%','92%','93%','94%','95%','96%','97%','98%','99%','100%') NOT NULL DEFAULT '20%',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `IDX` (`Engagement`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Engagement`
+--
+
+LOCK TABLES `Engagement` WRITE;
+/*!40000 ALTER TABLE `Engagement` DISABLE KEYS */;
+INSERT INTO `Engagement` VALUES (1,'20%'),(2,'60%'),(3,'0%'),(4,'80%');
+/*!40000 ALTER TABLE `Engagement` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -197,7 +222,7 @@ CREATE TABLE `MA` (
 
 LOCK TABLES `MA` WRITE;
 /*!40000 ALTER TABLE `MA` DISABLE KEYS */;
-INSERT INTO `MA` VALUES (4,'KrÃ¤mer','Steffi','weiblich'),(5,'BlumentÃ¶pfchen','Brunhilde','weiblich'),(6,'Mitarbeiterin01','Marta01','weiblich'),(7,'Mitarbeiterin02','Marta02','weiblich'),(8,'Mitarbeiterin03','Marta03','weiblich'),(9,'Mitarbeiterin04','Marta04','weiblich');
+INSERT INTO `MA` VALUES (4,'KrÃ¤mer','Steffi','weiblich'),(5,'BlumentÃ¶pfchen','Brunhilde','weiblich'),(6,'Amper','Susi','weiblich'),(7,'Kaufmann','Daniela','weiblich'),(8,'Lee','Mai','weiblich'),(9,'Oezdem','Maische','weiblich');
 /*!40000 ALTER TABLE `MA` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -270,7 +295,7 @@ CREATE TABLE `MAClientVS` (
   `ClientVSID` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`ID`),
   UNIQUE KEY `IDX` (`MAClientID`,`ClientVSID`)
-) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -279,7 +304,7 @@ CREATE TABLE `MAClientVS` (
 
 LOCK TABLES `MAClientVS` WRITE;
 /*!40000 ALTER TABLE `MAClientVS` DISABLE KEYS */;
-INSERT INTO `MAClientVS` VALUES (10,9,14),(11,10,10),(12,10,12),(14,10,13),(15,10,11),(16,9,15),(17,9,1);
+INSERT INTO `MAClientVS` VALUES (10,9,14),(11,10,10),(18,8,12),(14,10,13),(15,10,11),(16,9,15),(17,9,1);
 /*!40000 ALTER TABLE `MAClientVS` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -446,4 +471,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-05 11:24:35
+-- Dump completed on 2016-04-06 18:19:48
