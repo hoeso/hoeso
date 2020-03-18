b9e16bbaa52c87e71ef6038797247b6b19aa0da5
Hans <support@hans-hoermann.de>
2020-03-18 11:35:13 +0100

    Start-Menue ist Eingabe der Stammdaten - anstelle des Kalenders. Gesetzt in index.php

--- db/erzeugenUser.sql
+++ db/erzeugenUser.sql
@@ -2,12 +2,16 @@ use mysql;
 
 # /////////////// Tabelle 'user' //////////////////////////////////
 # ////////////////////////////////////////////////////////////////
-INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','zettl', password('straum'));
-INSERT IGNORE  INTO user (Host, User, Password) VALUES ('localhost','fcs', password(''));
+INSERT IGNORE  INTO user (Host, User) VALUES ('localhost','infar');
+INSERT IGNORE  INTO user (Host, User) VALUES ('localhost','ruby');
+UPDATE user SET authentication_string = PASSWORD('') WHERE 'infar'=user AND Host='localhost';
+UPDATE user SET authentication_string = PASSWORD('wrzlprmft') WHERE 'ruby'=user AND Host='localhost';
 
 # /////////////// Tabelle 'db' ////////////////////////////////////
 # ////////////////////////////////////////////////////////////////
-INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','cfs','zettl','Y','Y','Y','Y','Y');
-INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','cfs','fcs','Y','N','N','N','Y');
+# LOCK TABLES fuer mysqldump:
+INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','vpi','infar','Y','N','N','N','Y');
+INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','vpi','ruby','Y','Y','Y','Y','Y');
 
 FLUSH PRIVILEGES;
+
