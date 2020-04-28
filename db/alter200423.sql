b490a972c663e2d5340f71ec321b6865166c8144
Hans <support@hans-hoermann.de>
2020-04-28 20:31:38 +0200

    Vormerkung: Bei Navi Proband kann die Vormerkung des Probanden für alle Kursorte gelöscht werden. Navi Plan: Symbol zur Übernahme der Vormerkung in bestehenden Termin. Soll zur Anzeige aller Termine dieses Kursortes führen.

--- db/alter200423.sql
+++ db/alter200423.sql
@@ -1,9 +1,10 @@
-DROP TABLE IF EXISTS `Vormerkung`;
+ALTER TABLE Vormerkung ADD Wunschtermin varchar(120) DEFAULT NULL;
+ALTER TABLE Termin DROP VM;
+
 CREATE TABLE `Vormerkung` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `RegionID` int(11) NOT NULL DEFAULT 1,
-  `ProbandID` int(11) NOT NULL DEFAULT 1,
+  `ProbandAdresseID` int(11) NOT NULL DEFAULT 1,
   PRIMARY KEY (`ID`),
-  UNIQUE KEY `IDX` (`RegionID`,`ProbandID`)
+  UNIQUE KEY `IDX` (`RegionID`,`ProbandAdresseID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
