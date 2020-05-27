20138ee0d358051b8a941148dbf74a7c12fabd8c
Hans <support@hans-hoermann.de>
2020-05-27 23:01:10 +0200

    Trainer in NS-Anmeldung übernehmen.

--- db/alter200527.sql
+++ db/alter200527.sql
@@ -1,3 +1,4 @@
+ALTER TABLE Trainer ADD  Anrede enum('Herr','Frau') NOT NULL DEFAULT 'Frau' AFTER ID;
 ALTER TABLE Proband ADD Titel enum('','Mag.','Dr.','Ing.Mag.','DI','Prof.') NOT NULL DEFAULT '' AFTER Anrede;
 ALTER TABLE Terminart ADD Art enum('c','e','n') NOT NULL DEFAULT 'n';
 
