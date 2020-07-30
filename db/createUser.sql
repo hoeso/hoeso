8a34b0d134e81a2e6bb32e940b9b0f3c8a2e5424
Hans <support@hans-hoermann.de>
2020-07-30 18:33:12 +0200

    check-DB begonnen.

---
+++ db/createUser.sql
@@ -0,0 +1,14 @@
+use mysql;
+
+# /////////////// Tabelle 'user' //////////////////////////////////
+# ////////////////////////////////////////////////////////////////
+INSERT IGNORE  INTO user (Host, User) VALUES ('localhost','hfs');
+UPDATE user SET authentication_string = PASSWORD('') WHERE 'hfs'=user AND Host='localhost';
+
+# /////////////// Tabelle 'db' ////////////////////////////////////
+# ////////////////////////////////////////////////////////////////
+# LOCK TABLES fuer mysqldump:
+INSERT IGNORE  INTO db (Host, Db, User, Select_Priv, Insert_Priv, Update_Priv, Delete_Priv, Lock_tables_priv) VALUES ('localhost','office','hfs','Y','Y','Y','Y','Y');
+
+FLUSH PRIVILEGES;
+
