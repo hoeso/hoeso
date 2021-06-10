3054eb66056566ae8b536168d459198477b92fd7
Johann <44166287+hoeso@users.noreply.github.com>
2021-06-10 13:48:02 -0300

    Update hfs-2.sql

--- db/hfs-2.sql
+++ db/hfs-2.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `Station` (
+CREATE TABLE `District` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Station` varchar(255) DEFAULT NULL,
   `TLD` enum('ar') NOT NULL DEFAULT 'ar',
   PRIMARY KEY (`ID`),
-  UNIQUE KEY `IDX` (`Station`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  UNIQUE KEY `IDX` (`District`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8; 
