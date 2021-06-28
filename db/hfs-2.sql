b5f74600b73c31fba3723c42d33cd17f172f809d
Hans <support@hans-hoermann.de>
2021-06-28 10:41:09 +0200

    Spalte Station umbenannt in District.

--- db/hfs-2.sql
+++ db/hfs-2.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `District` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Station` varchar(255) DEFAULT NULL,
+  `District` varchar(255) DEFAULT NULL,
   `TLD` enum('ar') NOT NULL DEFAULT 'ar',
   PRIMARY KEY (`ID`),
   UNIQUE KEY `IDX` (`District`)
